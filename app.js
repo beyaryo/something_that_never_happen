4e388cf86f54764d8b93eb4ba74988841cc2527f
Bey <beyaryo@gmail.com>
2017-03-25 10:30:46 +0700

    Adding get and post method

--- app.js
+++ app.js
@@ -1,21 +1,79 @@
+/**
+ * Init Express for http framework
+ * Init body-parser for receive POST data
+ * Init morgan for unit testing
+ */
 var express = require('express');
+var logger = require('morgan');
+var parser = require('body-parser');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 46195));       // set port to run into
+app.use(express.static(__dirname + '/public'));     // server root dir
+app.use(logger('dev'));                             // unit testing
+server.use(parser.json());                          // support json encoded body
+server.use(parser.urlencoded({extended : true}));   // support encoded body
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-app.get('/', function(req, res) {
-    res.send("Init apps");
-    // response.render('pages/index');
-});
-
+/**
+ * Run server
+ */
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
+/**
+ * User do GET request
+ */
+server.get("/api/user", function(req, res){
+    var token = req.query.token;
+    var name = req.query.name;
+    var name = (typeof req.query.name == "undefinied" ? "Mbret" : req.query.name);
 
+    if(req.query.name == null){
+        res.status(404);
+        res.send("Page not found!");
+    }else{
+        res.status(200)
+        res.send(name);
+    }
+
+    // res.json({
+    //     token : token,
+    //     name : name,
+    //     message : "Hi GET!"
+    // });
+});
+
+/**
+ * User do POST request
+ */
+server.post("/api/user", function(req, res){
+    var token = req.body.token;
+    var name = req.body.name;
+
+    res.json({
+        token : token,
+        name : name,
+        message : "Hi POST!"
+    });
+});
+
+/**
+ * Middleware for spesific name
+ */
+server.param('name', function(req, res, next, name){
+    if(name == "bey"){
+        req.message = "Hi bey, how's your doing?";
+    }else{
+        req.message = "Who are you?";
+    }
+
+    next();
+});
+
+/**
+ * User do GET request for spesific name
+ */
+server.get("/api/user/:name", function(req, res){
+    res.send(req.message);
+});
