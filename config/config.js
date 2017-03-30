a13f0de00347f4cb52af3bb5c30250af51a06424
Bey <beyaryo@gmail.com>
2017-03-30 07:50:11 +0700

    test mlab

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  database: process.env.MONGO_URI || 'localhost:26124/sampledb',
+  // database: process.env.MONGO_URI || 'localhost:26124/sampledb',
+  database: process.env.MONGO_URI || 'mongodb://beyaryo:initakumanatamu@ds147070.mlab.com:47070/ta',
   hostedServerUrl: "https://myfis.herokuapp.com/",
   firebaseServerKey: "AIzaSyCVRDZQRhLADH1EJrhVhumaN_N_TS3vTEc",
   firebaseService: "/config/myfiv-d9c0e-firebase-adminsdk-1h5ug-731c8c6478.json",
